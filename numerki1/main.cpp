db26aa0d4b47c42a9c788e558d39f4d8ff5420a9
Falitac <fastkonrad@gmail.com>
2022-10-31 12:39:59 +0100

    test2.txt typo, jacobi start

--- numerki1/main.cpp
+++ numerki1/main.cpp
@@ -4,25 +4,23 @@
 
 using Matrix = std::vector<std::vector<float>>;
 
-void gauss(Matrix& a, std::vector<float>& b, std::vector<float>&x) {
-	int n = a.size();
-	for (int i = 0; i < n - 1; i++) {
-		for (int j = i + 1; j < n; j++) {
-			float m = a[j][i] / a[i][i];
-			b[j] -= b[i] * m;
-			for (int k = j; k < n; k++) {
-				a[j][k] -= a[i][k] * m;
-			}
+// Klasa pomocnicza do reprezentacji macierzy. 
+namespace fil {
+	class Matrix {
+	public:
+		Matrix(size_t row, size_t col, float val = 0.f)
+		: data(row * col, val)
+		, row(row)
+		, col(col) {
 		}
-	}
-	// obliczanie zmiennych:
-	for (int i = n - 1; i >= 0; i--) {
-		x[i] = b[i];
-		for (int j = n - 1; j > i; j--) {
-			x[i] -= a[i][j] * x[j];
+		float* operator[](size_t n) {
+			return &data[n * col];
 		}
-		x[i] /= a[i][i];
-	}
+	private:
+		size_t row;
+		size_t col;
+		std::vector<float> data;
+	};
 }
 
 void print(std::vector<float>& a) {
@@ -37,13 +35,6 @@ void print(Matrix& a) {
 			std::printf("%5.2f ", a[i][j]);
 		}
 		std::printf("\n");
-	}
-}
-
-void jacob(Matrix& a, std::vector<float>& b, std::vector<float>& x, int iters) {
-	int n = a.size();
-	for (int it = 0; it < iters; it++) {
-
 	}
 }
 
@@ -65,9 +56,77 @@ bool isWeakDominant(Matrix& a) {
 	}
 	return good;
 }
+void gaussEliminateToUpper(Matrix& a, std::vector<float>& b) {
+	int n = a.size();
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = i + 1; j < n; j++) {
+			float m = a[j][i] / a[i][i];
+			b[j] -= b[i] * m;
+			for (int k = i; k < n; k++) {
+				a[j][k] -= a[i][k] * m;
+			}
+		}
+	}
+}
+
+// obliczanie zmiennych:
+std::vector<float> solveUpper(Matrix& a, std::vector<float>& b) {
+	int n = a.size();
+	std::vector<float> x(n);
+
+	for (int i = n - 1; i >= 0; i--) {
+		x[i] = b[i];
+		for (int j = n - 1; j > i; j--) {
+			x[i] -= a[i][j] * x[j];
+		}
+		x[i] /= a[i][i];
+	}
+	return x;
+}
+
+std::vector<float> jacob(Matrix& a, std::vector<float>& b, int iters) {
+	int n = a.size();
+	Matrix LU = a;
+	std::vector<float> x(n, 0.f);
+	std::vector<float> d(n);
+	for (int i = 0; i < n; i++) {
+		d[i] = 1 / a[i][i];
+		LU[i][i] = 0.f;
+	}
+	std::printf("D:\n");
+	print(d);
+	std::printf("LU:\n");
+	print(LU);
+
+	Matrix dlu = std::vector<std::vector<float>>(n, std::vector<float>(n, 0.0f));
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			dlu[i][j] = -d[i] * LU[i][j];
+		}
+	}
+	std::printf("DLU:\n");
+	print(dlu);
+	std::vector<float> db = b;
+	for (int i = 0; i < n; i++) {
+		db[i] *= d[i];
+	}
+	std::printf("D^-1*B:\n");
+	print(db);
+	for (int it = 0; it < iters; it++) {
+		//x_i+1 = -(d * LU) * x_i + d*b
+		std::vector<float> tempx = x;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				x[i] += dlu[i][j] * tempx[i];
+			}
+			x[i] += db[i];
+		}
+	}
+	return x;
+}
 
 int main() {
-	std::fstream input("RURL_dane2.txt");
+	std::fstream input("test2.txt");
 	if (!input.good()) {
 		std::printf("nope\n");
 		return 1;
@@ -77,7 +136,6 @@ int main() {
 	Matrix m = std::vector<std::vector<float>>(n, std::vector<float>(n, 0.0f));
 
 	std::vector<float> b(n);
-	std::vector<float> x(n, 0.f);
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			input >> m[i][j];
@@ -93,13 +151,16 @@ int main() {
 	print(m);
 	std::printf("Wektor wyr. wolnych:\n");
 	print(b);
-	std::printf("Czy s³abo dom.?: %s", isWeakDominant(m) ? "Tak" : "Nie");
+	std::printf("Czy slabo dom.?: %s\n", isWeakDominant(m) ? "Tak" : "Nie");
 
-	std::puts("Po obl.:\n");
+	//gaussEliminateToUpper(m, b);
+	//auto x = solveUpper(m, b);
+	auto x = jacob(m, b, 5);
+
+	std::puts("Macierz U:");
 	print(m);
 	std::printf("Wektor wyr. wolnych:\n");
 	print(b);
 	std::printf("Wynik:\n");
 	print(x);
-
 }
